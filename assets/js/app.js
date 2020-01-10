e0623c3a702fac2138cf32e4ea38fcc6526d43b3
Laurent <laurent@lepl.at>
2020-01-10 15:30:34 +0100

    homePage

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+import '../css/app.scss';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
